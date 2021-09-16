6f1bd58d1ca69176f71314ab7214fba8ede3114c
kawsar <kawsar@mail.com>
2021-09-17 03:13:29 +0600

    added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <v-app>
+ 
     
-
+<!---
+ <v-app>
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
+  ---->
+  <div class="main">
+    <nav class="navigation">
+      <div class="logo">
+        <h4>Skybd</h4>
+      </div>
+      <div class="menu">
+        <div>
+          {{age}}
+        </div>
+        <ul>
+          <li v-for="num in 100" :key="num.id">{{num}}</li>
+          <li>{{gf}}</li>
+          
+        </ul>
+      </div>
+      
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -15,6 +35,18 @@ export default {
 
   data: () => ({
     //
+    gf:"tamanna",
+    age:16
   }),
+  mounted(){
+    setInterval(function(){
+      this.age++
+    },100)
+  }
 };
 </script>
+<style type="text/css" media="all">
+  .navigation{
+    background: #ccc
+  }
+</style>
